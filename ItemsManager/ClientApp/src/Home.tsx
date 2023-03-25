439d5d7fc2a0a9c0a3f8d1ca9fc09d4996f3c29a
Fabian <f.grochla@inex.com.pl>
2023-03-25 11:31:42 +0100

    Install table

--- ItemsManager/ClientApp/src/Home.tsx
+++ ItemsManager/ClientApp/src/Home.tsx
@@ -1,12 +1,75 @@
-import { Button } from "react-bootstrap";
+import * as React from "react";
 
-const Home: React.FC = () => {
-    return (
-        <div className="d-flex h-100 justify-content-center align-items-center">
-            <div>
-                <Button variant="primary">Primary</Button>
-            </div>
-        </div>
-    );
+import {
+    Column,
+    CompactTable,
+} from "@table-library/react-table-library/compact";
+import { useTheme } from "@table-library/react-table-library/theme";
+import { getTheme } from "@table-library/react-table-library/baseline";
+
+import { TableNode } from "@table-library/react-table-library/types/table";
+
+const key = "Compact Table";
+
+interface User {
+    name: string;
+    surname: string;
+    age: number;
+}
+
+const nodes = [
+    {
+        id: "0",
+        name: "Shopping List",
+        deadline: new Date(2020, 1, 15),
+        type: "TASK",
+        isComplete: true,
+        nodes: 3,
+    },
+    {
+        id: "0",
+        name: "Shopping List",
+        deadline: new Date(2020, 1, 15),
+        type: "TASK",
+        isComplete: true,
+        nodes: 3,
+    },
+    {
+        id: "0",
+        name: "Shopping List",
+        deadline: new Date(2020, 1, 15),
+        type: "TASK",
+        isComplete: true,
+        nodes: 3,
+    },
+];
+
+const Home = () => {
+    const data = { nodes };
+
+    const theme = useTheme(getTheme());
+
+    const dattt = [{}] as Column<TableNode>[];
+
+    const COLUMNS = [
+        { label: "Task", renderCell: (item) => item.name },
+        {
+            label: "Deadline",
+            renderCell: (item) =>
+                item.deadline.toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                }),
+        },
+        { label: "Type", renderCell: (item) => item.type },
+        {
+            label: "Complete",
+            renderCell: (item) => item.isComplete.toString(),
+        },
+        { label: "Tasks", renderCell: (item) => item.nodes?.length },
+    ] as Column<TableNode>[];
+
+    return <CompactTable columns={COLUMNS} data={data} theme={theme} />;
 };
 export default Home;
