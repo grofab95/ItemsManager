8e42b7aac55f0e9aca157420ba6503b3f6c53a62
Fabian <f.grochla@inex.com.pl>
2023-04-05 21:59:39 +0200

    Remove redundant method override in UserEventsHub

--- ItemsManager.Api/Users/Events/UserEventsHub.cs
+++ ItemsManager.Api/Users/Events/UserEventsHub.cs
@@ -5,8 +5,5 @@ namespace ItemsManager.Api.Users.Events;
 [Authorize]
 public class UserEventsHub : Microsoft.AspNetCore.SignalR.Hub<IUserHubClient>
 {
-    public override Task OnConnectedAsync()
-    {
-        return base.OnConnectedAsync();
-    }
+    
 }
